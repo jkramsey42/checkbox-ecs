2ef8f2445de4e96e8e7776093f89453ef4de315a
jkramsey42 <38295193+jkramsey42@users.noreply.github.com>
2026-06-26 15:15:01 -0700

    Update app.py

--- app.py
+++ app.py
@@ -60,7 +60,10 @@ def checkbox_webhook():
     row = [
         datetime.now(timezone.utc).isoformat(),
         get_value(data, "survey_name", "SurveyName", "surveyTitle"),
-        get_value(data, "response_id", "ResponseID", "responseId"),
+        get_value(data, "respondentID", "respondentid", "respondentid"),
+        get_value(data, "orgname", "orgname", "orgname"),
+        get_value(data, "jobcat", "jobcat", "jobcat"),
+
         get_value(data, "first_name", "FirstName", "firstName"),
         get_value(data, "last_name", "LastName", "lastName"),
         get_value(data, "email", "Email", "email"),
